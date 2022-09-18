d80a0b84e29951640c2e0b6433584344ea792499
modongsong <modongsong@126.com>
2022-09-18 21:35:32 +0800

    Updated:

--- content/gst-example/examples/bus_example.c
+++ content/gst-example/examples/bus_example.c
@@ -2,31 +2,30 @@
 
 static GMainLoop *loop;
 
-static gboolean
-my_bus_callback (GstBus * bus, GstMessage * message, gpointer data)
-{
-  g_print ("Got %s message\n", GST_MESSAGE_TYPE_NAME (message));
+static gboolean my_bus_callback(GstBus *bus, GstMessage *message,
+                                gpointer data) {
+  g_print("Got %s message\n", GST_MESSAGE_TYPE_NAME(message));
 
-  switch (GST_MESSAGE_TYPE (message)) {
-    case GST_MESSAGE_ERROR:{
-      GError *err;
-      gchar *debug;
+  switch (GST_MESSAGE_TYPE(message)) {
+  case GST_MESSAGE_ERROR: {
+    GError *err;
+    gchar *debug;
 
-      gst_message_parse_error (message, &err, &debug);
-      g_print ("Error: %s\n", err->message);
-      g_error_free (err);
-      g_free (debug);
+    gst_message_parse_error(message, &err, &debug);
+    g_print("Error: %s\n", err->message);
+    g_error_free(err);
+    g_free(debug);
 
-      g_main_loop_quit (loop);
-      break;
-    }
-    case GST_MESSAGE_EOS:
-      /* end-of-stream */
-      g_main_loop_quit (loop);
-      break;
-    default:
-      /* unhandled message */
-      break;
+    g_main_loop_quit(loop);
+    break;
+  }
+  case GST_MESSAGE_EOS:
+    /* end-of-stream */
+    g_main_loop_quit(loop);
+    break;
+  default:
+    /* unhandled message */
+    break;
   }
 
   /* we want to be notified again the next time there is a message
@@ -36,26 +35,24 @@ my_bus_callback (GstBus * bus, GstMessage * message, gpointer data)
   return TRUE;
 }
 
-gint
-main (gint argc, gchar * argv[])
-{
+gint main(gint argc, gchar *argv[]) {
   GstElement *pipeline;
   GstBus *bus;
   guint bus_watch_id;
 
   /* init */
-  gst_init (&argc, &argv);
+  gst_init(&argc, &argv);
 
   /* create pipeline, add handler */
-  pipeline = gst_pipeline_new ("my_pipeline");
+  pipeline = gst_pipeline_new("my_pipeline");
 
   /* adds a watch for new message on our pipeline's message bus to
    * the default GLib main context, which is the main context that our
    * GLib main loop is attached to below
    */
-  bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
-  bus_watch_id = gst_bus_add_watch (bus, my_bus_callback, NULL);
-  gst_object_unref (bus);
+  bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline));
+  bus_watch_id = gst_bus_add_watch(bus, my_bus_callback, NULL);
+  gst_object_unref(bus);
 
   /* [...] */
 
@@ -66,14 +63,14 @@ main (gint argc, gchar * argv[])
    * my_bus_callback() function to notify us of that message.
    * The main loop will be run until someone calls g_main_loop_quit()
    */
-  loop = g_main_loop_new (NULL, FALSE);
-  g_main_loop_run (loop);
+  loop = g_main_loop_new(NULL, FALSE);
+  g_main_loop_run(loop);
 
   /* clean up */
-  gst_element_set_state (pipeline, GST_STATE_NULL);
-  gst_object_unref (pipeline);
-  g_source_remove (bus_watch_id);
-  g_main_loop_unref (loop);
+  gst_element_set_state(pipeline, GST_STATE_NULL);
+  gst_object_unref(pipeline);
+  g_source_remove(bus_watch_id);
+  g_main_loop_unref(loop);
 
   return 0;
 }
