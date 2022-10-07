67de17c8c9ddd4bc7a231ba1f61c0e5b3c6c2629
modongsong <modongsong@126.com>
2022-10-07 21:43:36 +0800

    Updated:

--- content/gst-example/examples/tutorials/basic-tutorial-1.c
+++ content/gst-example/examples/tutorials/basic-tutorial-1.c
@@ -1,40 +1,37 @@
 #include <gst/gst.h>
 
-int
-main (int argc, char *argv[])
-{
-  GstElement *pipeline;
-  GstBus *bus;
-  GstMessage *msg;
+int main(int argc, char *argv[]) {
+    GstElement *pipeline;
+    GstBus *bus;
+    GstMessage *msg;
 
-  /* Initialize GStreamer */
-  gst_init (&argc, &argv);
+    /* Initialize GStreamer */
+    gst_init(&argc, &argv);
 
-  /* Build the pipeline */
-  pipeline =
-      gst_parse_launch
-      ("playbin uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm",
-      NULL);
+    gchar *file1="playbin uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm";
+    gchar *file2="playbin uri=file:////home/modongsong/data/test-data/sintel_trailer-480p.webm";
 
-  /* Start playing */
-  gst_element_set_state (pipeline, GST_STATE_PLAYING);
+    /* Build the pipeline */
+    pipeline =gst_parse_launch(file2,NULL);
 
-  /* Wait until error or EOS */
-  bus = gst_element_get_bus (pipeline);
-  msg =
-      gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE,
-      GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+    /* Start playing */
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);
 
-  /* See next tutorial for proper error message handling/parsing */
-  if (GST_MESSAGE_TYPE (msg) == GST_MESSAGE_ERROR) {
-    g_error ("An error occurred! Re-run with the GST_DEBUG=*:WARN environment "
-        "variable set for more details.");
-  }
+    /* Wait until error or EOS */
+    bus = gst_element_get_bus(pipeline);
+    msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
+                                     GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
 
-  /* Free resources */
-  gst_message_unref (msg);
-  gst_object_unref (bus);
-  gst_element_set_state (pipeline, GST_STATE_NULL);
-  gst_object_unref (pipeline);
-  return 0;
+    /* See next tutorial for proper error message handling/parsing */
+    if (GST_MESSAGE_TYPE(msg) == GST_MESSAGE_ERROR) {
+        g_error("An error occurred! Re-run with the GST_DEBUG=*:WARN environment "
+                "variable set for more details.");
+    }
+
+    /* Free resources */
+    gst_message_unref(msg);
+    gst_object_unref(bus);
+    gst_element_set_state(pipeline, GST_STATE_NULL);
+    gst_object_unref(pipeline);
+    return 0;
 }
--- content/gst-example/playback/player/gst-play/gst-play-kb.c
+++ content/gst-example/playback/player/gst-play/gst-play-kb.c
@@ -30,8 +30,10 @@
 #include <string.h>
 
 #ifdef G_OS_UNIX
+
 #include <unistd.h>
 #include <termios.h>
+
 #endif
 
 #include <gst/gst.h>
@@ -47,88 +49,86 @@ static gpointer kb_callback_data;
 static gulong io_watch_id;
 
 static gboolean
-gst_play_kb_io_cb (GIOChannel * ioc, GIOCondition cond, gpointer user_data)
-{
-  GIOStatus status;
+gst_play_kb_io_cb(GIOChannel *ioc, GIOCondition cond, gpointer user_data) {
+    GIOStatus status;
 
-  if (cond & G_IO_IN) {
-    gchar buf[16] = { 0, };
-    gsize read;
+    if (cond & G_IO_IN) {
+        gchar buf[16] = {0,};
+        gsize read;
 
-    status = g_io_channel_read_chars (ioc, buf, sizeof (buf) - 1, &read, NULL);
-    if (status == G_IO_STATUS_ERROR)
-      return FALSE;
-    if (status == G_IO_STATUS_NORMAL) {
-      if (kb_callback)
-        kb_callback (buf, kb_callback_data);
+        status = g_io_channel_read_chars(ioc, buf, sizeof(buf) - 1, &read, NULL);
+        if (status == G_IO_STATUS_ERROR)
+            return FALSE;
+        if (status == G_IO_STATUS_NORMAL) {
+            if (kb_callback)
+                kb_callback(buf, kb_callback_data);
+        }
     }
-  }
 
-  return TRUE;                  /* call us again */
+    return TRUE;                  /* call us again */
 }
 
 gboolean
-gst_play_kb_set_key_handler (GstPlayKbFunc kb_func, gpointer user_data)
-{
-  GIOChannel *ioc;
-  int flags;
+gst_play_kb_set_key_handler(GstPlayKbFunc kb_func, gpointer user_data) {
+    GIOChannel *ioc;
+    int flags;
 
-  if (!isatty (STDIN_FILENO)) {
-    GST_INFO ("stdin is not connected to a terminal");
-    return FALSE;
-  }
-
-  if (io_watch_id > 0) {
-    g_source_remove (io_watch_id);
-    io_watch_id = 0;
-  }
-
-  if (kb_func == NULL && term_settings_saved) {
-    /* restore terminal settings */
-    if (tcsetattr (STDIN_FILENO, TCSAFLUSH, &term_settings) == 0)
-      term_settings_saved = FALSE;
-    else
-      g_warning ("could not restore terminal attributes");
-
-    setvbuf (stdin, NULL, _IOLBF, 0);
-  }
-
-  if (kb_func != NULL) {
-    struct termios new_settings;
-
-    if (!term_settings_saved) {
-      if (tcgetattr (STDIN_FILENO, &term_settings) != 0) {
-        g_warning ("could not save terminal attributes");
+    if (!isatty(STDIN_FILENO)) {
+        GST_INFO ("stdin is not connected to a terminal");
         return FALSE;
-      }
-      term_settings_saved = TRUE;
-
-      /* Echo off, canonical mode off, extended input processing off  */
-      new_settings = term_settings;
-      new_settings.c_lflag &= ~(ECHO | ICANON | IEXTEN);
-
-      if (tcsetattr (STDIN_FILENO, TCSAFLUSH, &new_settings) != 0) {
-        g_warning ("Could not set terminal state");
-        return FALSE;
-      }
-      setvbuf (stdin, NULL, _IONBF, 0);
     }
-  }
 
-  ioc = g_io_channel_unix_new (STDIN_FILENO);
+    if (io_watch_id > 0) {
+        g_source_remove(io_watch_id);
+        io_watch_id = 0;
+    }
 
-  /* make non-blocking */
-  flags = g_io_channel_get_flags (ioc);
-  g_io_channel_set_flags (ioc, flags | G_IO_FLAG_NONBLOCK, NULL);
+    if (kb_func == NULL && term_settings_saved) {
+        /* restore terminal settings */
+        if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &term_settings) == 0)
+            term_settings_saved = FALSE;
+        else
+            g_warning ("could not restore terminal attributes");
 
-  io_watch_id = g_io_add_watch_full (ioc, G_PRIORITY_DEFAULT, G_IO_IN,
-      (GIOFunc) gst_play_kb_io_cb, user_data, NULL);
-  g_io_channel_unref (ioc);
+        setvbuf(stdin, NULL, _IOLBF, 0);
+    }
 
-  kb_callback = kb_func;
-  kb_callback_data = user_data;
+    if (kb_func != NULL) {
+        struct termios new_settings;
 
-  return TRUE;
+        if (!term_settings_saved) {
+            if (tcgetattr(STDIN_FILENO, &term_settings) != 0) {
+                g_warning ("could not save terminal attributes");
+                return FALSE;
+            }
+            term_settings_saved = TRUE;
+
+            /* Echo off, canonical mode off, extended input processing off  */
+            new_settings = term_settings;
+            new_settings.c_lflag &= ~(ECHO | ICANON | IEXTEN);
+
+            if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &new_settings) != 0) {
+                g_warning ("Could not set terminal state");
+                return FALSE;
+            }
+            setvbuf(stdin, NULL, _IONBF, 0);
+        }
+    }
+
+    ioc = g_io_channel_unix_new(STDIN_FILENO);
+
+    /* make non-blocking */
+    flags = g_io_channel_get_flags(ioc);
+    g_io_channel_set_flags(ioc, flags | G_IO_FLAG_NONBLOCK, NULL);
+
+    io_watch_id = g_io_add_watch_full(ioc, G_PRIORITY_DEFAULT, G_IO_IN,
+                                      (GIOFunc) gst_play_kb_io_cb, user_data, NULL);
+    g_io_channel_unref(ioc);
+
+    kb_callback = kb_func;
+    kb_callback_data = user_data;
+
+    return TRUE;
 }
 
 #else /* !G_OS_UNIX */
